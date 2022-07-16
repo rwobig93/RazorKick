3fc0a8b11032ec0f141788967b38dbcd84ecda85
Rick Wobig <rickwobig93@gmail.com>
2022-07-16 18:47:02 -0500

    Selectable Themes, Connectivity Warning  - Add dynamic themes to select from  - Add custom connectivity warning and options for better UX

--- TestBlazorServerApp/Shared/MainLayout.razor.cs
+++ TestBlazorServerApp/Shared/MainLayout.razor.cs
@@ -1,11 +1,33 @@
-﻿namespace TestBlazorServerApp.Shared;
+﻿using System.Reflection;
+using MudBlazor;
+using Shared.Models.Base;
+using TestBlazorServerApp.Settings;
+
+namespace TestBlazorServerApp.Shared;
 
 public partial class MainLayout
 {
+    private static string ApplicationName => Assembly.GetExecutingAssembly().GetName().Name ?? "TestBlazorServerApp";
     private bool _drawerOpen = true;
+    private MudTheme _selectedTheme = AppThemes.DarkTheme.Theme;
 
     private void DrawerToggle()
     {
         _drawerOpen = !_drawerOpen;
+    }
+
+    private void ChangeTheme(MudTheme theme)
+    {
+        _selectedTheme = theme;
+    }
+    
+    private static List<AppTheme> GetAvailableThemes()
+    {
+        var fields = typeof(AppThemes)
+            .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+        return (from fi in fields select fi.GetValue(null)
+            into propertyValue
+            where propertyValue is not null select (AppTheme)propertyValue).ToList()!;
     }
 }
